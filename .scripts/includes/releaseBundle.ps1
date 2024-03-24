9014dff59793705fb31eb4a4e05384bae8fe3527
Matheus Castello <matheus@castello.eng.br>
2024-03-24 20:04:48 -0300

    scripts: Fix for the use of machine.variant and deploy xenomai
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- .scripts/includes/releaseBundle.ps1
+++ .scripts/includes/releaseBundle.ps1
@@ -98,44 +98,44 @@ $Global:_os_list_imagingutility = [PSCustomObject]@{
 function releaseBundle ($machine, $tag) {
     # go to the yocto deploy dir if it's exist
     if (
-        -not (Test-Path /workdir/torizon/build-torizon/deploy/images/$($machine.machine))
+        -not (Test-Path /workdir/torizon/$($machine.build_dir)/deploy/images/$($machine.machine))
     ) {
         Write-Host -ForegroundColor DarkYellow `
-            "No deploy dir found for $($machine.machine), returning ..."
+            "No deploy dir found for $($machine.title), returning ..."
         return
     }
 
-    Set-Location /workdir/torizon/build-torizon/deploy/images/$($machine.machine)
+    Set-Location /workdir/torizon/$($machine.build_dir)/deploy/images/$($machine.machine)
 
     if (
         $null -ne $machine.dump_image -and
         $machine.dump_image -eq $true
     ) {
         # for the raspberry pies we need to create a .img from the .wic
-        Write-Host "Creating .img from torizon-core-common-docker-dev-$($machine.machine).$($machine.machine).$($machine.image_format) ..."
+        Write-Host "Creating .img from torizon-core-common-docker-dev$($machine.variant)-$($machine.machine).$($machine.image_format) ..."
         # deference the link
-        $_wicPath = readlink torizon-core-common-docker-dev-$($machine.machine).$($machine.image_format)
+        $_wicPath = readlink torizon-core-common-docker-dev$($machine.variant)-$($machine.machine).$($machine.image_format)
         # create the .img
-        $_imgPath = "torizon-core-common-docker-dev-$tag-$($machine.machine).img"
+        $_imgPath = "torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).img"
         bmaptool copy $_wicPath $_imgPath
         # compress the .img
-        Write-Host "Compressing torizon-core-common-docker-dev-$tag-$($machine.machine).img ..."
+        Write-Host "Compressing torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).img ..."
         7z a -tzip -bsp1 `
-            /releases/torizon-core-common-docker-dev-$tag-$($machine.machine).zip `
+            /releases/torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).zip `
             $_imgPath
 
         # get the .img size in bytes
-        $_imgSize = (Get-Item "torizon-core-common-docker-dev-$tag-$($machine.machine).img").Length
+        $_imgSize = (Get-Item "torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).img").Length
         # get the size of the .zip
-        $_zipSize = (Get-Item "/releases/torizon-core-common-docker-dev-$tag-$($machine.machine).zip").Length
+        $_zipSize = (Get-Item "/releases/torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).zip").Length
 
         # date of the release in format YYYY-MM-DD
-        $releaseDate = (Get-Item "torizon-core-common-docker-dev-$tag-$($machine.machine).img")
+        $releaseDate = (Get-Item "torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).img")
         $releaseDate = $releaseDate.LastWriteTime.ToString("yyyy-MM-dd")
 
         # get the sha256sum from the .img
         $sha256sum = Get-FileHash `
-            -Path "torizon-core-common-docker-dev-$tag-$($machine.machine).img" `
+            -Path "torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).img" `
             -Algorithm SHA256 `
                 | Select-Object -ExpandProperty Hash
         # set it to lowercase, Raspberry Pi Imager needs it
@@ -147,9 +147,9 @@ function releaseBundle ($machine, $tag) {
         # add it to the os_list_imagingutility.json
         $Global:_os_list_imagingutility.os_list[0].subitems += [PSCustomObject]@{
             "name" = "Torizon OS $tag"
-            "description" = "TorizonCore $tag for $($machine.machine)"
+            "description" = "TorizonCore $tag for $($machine.title)"
             "icon" = "https://docs.toradex.com/111487-torizon.png"
-            "url" = "https://github.com/commontorizon/meta-common-torizon/releases/download/$tag/torizon-core-common-docker-dev-$tag-$($machine.machine).zip"
+            "url" = "https://github.com/commontorizon/meta-common-torizon/releases/download/$tag/torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).zip"
             "extract_size" = $_imgSize
             "extract_sha256" = "$sha256sum"
             "image_download_size" = $_zipSize
@@ -161,31 +161,31 @@ function releaseBundle ($machine, $tag) {
         return
     } else {
         # create the zip from the .wic file
-        Write-Host "Compressing torizon-core-common-docker-dev-$($machine.machine).$($machine.machine).$($machine.image_format) ..."
+        Write-Host "Compressing torizon-core-common-docker-dev$($machine.variant)-$($machine.machine).$($machine.image_format) ..."
         # deference the link
-        $_wicPath = readlink torizon-core-common-docker-dev-$($machine.machine).$($machine.image_format)
+        $_wicPath = readlink torizon-core-common-docker-dev$($machine.variant)-$($machine.machine).$($machine.image_format)
         7z a -tzip -bsp1 `
-            /releases/torizon-core-common-docker-dev-$tag-$($machine.machine).zip `
+            /releases/torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).zip `
             $_wicPath
     }
 
     if (
         $machine.bmap -eq $true
     ) {
-        Write-Host "Compressing torizon-core-common-docker-dev-$($machine.machine).wic.bmap ..."
+        Write-Host "Compressing torizon-core-common-docker-dev$($machine.variant)-$($machine.machine).wic.bmap ..."
         # deference the link
-        $_bmapPath = readlink torizon-core-common-docker-dev-$($machine.machine).wic.bmap
+        $_bmapPath = readlink torizon-core-common-docker-dev$($machine.variant)-$($machine.machine).wic.bmap
         7z u -tzip -bsp1 `
-            /releases/torizon-core-common-docker-dev-$tag-$($machine.machine).zip `
+            /releases/torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).zip `
             $_bmapPath
     }
 
     if (
         $null -ne $machine.firmware
     ) {
-        Write-Host "Compressing firmware $($machine.machine) ..."
+        Write-Host "Compressing firmware $($machine.title) ..."
         7z u -tzip -bsp1 `
-            /releases/torizon-core-common-docker-dev-$tag-$($machine.machine).zip `
+            /releases/torizon-core-common-docker-dev$($machine.variant)-$tag-$($machine.machine).zip `
             $machine.firmware
     }
 
